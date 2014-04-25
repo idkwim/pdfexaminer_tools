357001062ef0a6a01bd7a135ed67618f5601d698
mwtracker <tyler@malwaretracker.com>
2014-04-24 23:48:40 -0400

    Create pdfex_api_upload.php
    pull from source

---
+++ pdfex_api_upload.php
@@ -0,0 +1,55 @@
+<?PHP
+/*
+ * v1.2.0
+ * mwtfile.php: MalwareTracker.com PDFExaminer - sample api uploader
+ * Upload PDFs from the command line. Edit CURLOPT_URL for your internal url.
+ */
+
+
+if (!isset($argv[1])) {
+	echo "Specify a file.\n";
+	exit(0);
+}
+
+
+//accept a file as input
+if (is_file($argv[1])) {
+	$file = $argv[1];
+	$email = '';
+	if (isset($argv[2]))
+		$email = $argv[2];
+	$result = mwtfile($file, $email);
+	echo $result;
+}
+
+
+
+function mwtfile($file, $email = '', $message = ''){
+	$curl = curl_init();
+	curl_setopt($curl, CURLOPT_URL, "https://www.malwaretracker.com/pdfapi.php");
+	curl_setopt($curl, CURLOPT_POST, true);
+	curl_setopt($curl, CURLOPT_VERBOSE, 0);
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1); 
+	curl_setopt($curl, CURLOPT_HTTPHEADER, array('Expect:'));
+	curl_setopt($curl, CURLOPT_HEADER, 0); 
+	curl_setopt($curl, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible;) MWT API 1.0");
+
+	curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1); 
+	$data = array( "sample[]"=> "@$file", 'type' => 'xml');
+	if ($message != '')
+		$data['message'] = $message;
+	if ($email != '')
+		$data['email'] = $email;
+
+	curl_setopt($curl, CURLOPT_POSTFIELDS, $data); 
+	$response = curl_exec($curl);
+	$err = curl_error($curl); 
+	if ($err != '') {
+		return "CURLERROR: $err"; 
+	}
+	curl_close ($curl);
+	return $response;
+}
+
+
+?>
